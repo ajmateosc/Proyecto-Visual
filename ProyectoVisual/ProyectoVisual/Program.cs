3f74075c140807e5af0951392a6312f7d8b92c98
emilio86 <epascualg@salesianospizarrales.com>
2017-11-17 17:29:20 +0100

    JuegoESo cerrar ventana
    Añadido se cierre ventana login al iniciar sesion y pasar a la ventana principal

--- ProyectoVisual/ProyectoVisual/Program.cs
+++ ProyectoVisual/ProyectoVisual/Program.cs
@@ -16,7 +16,23 @@ namespace ProyectoVisual
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new login.Form1());
+            login.Form1 main = new login.Form1();
+            main.FormClosed += MainForm_Closed;
+            main.Show();
+            Application.Run();
+        }
+        private static void MainForm_Closed(object sender, FormClosedEventArgs e)
+        {
+            ((Form)sender).FormClosed -= MainForm_Closed;
+
+            if (Application.OpenForms.Count == 0)
+            {
+                Application.ExitThread();
+            }
+            else
+            {
+                Application.OpenForms[0].FormClosed += MainForm_Closed;
+            }
         }
     }
 }
